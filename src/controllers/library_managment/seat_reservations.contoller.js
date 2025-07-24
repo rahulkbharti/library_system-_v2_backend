8217493410332c326fd41fef77579205c10e2b97
Rahul Kumar Bharti <rk2255p@gmail.com>
2025-07-24 16:59:14 +0530

    A little Typo Corrected

--- src/controllers/library_managment/seat_reservations.contoller.js
+++ src/controllers/library_managment/seat_reservations.contoller.js
@@ -9,49 +9,62 @@ const SeatReservationController = {
     }
     return res
       .status(201)
-      .json({ message: "Seat Reservation added successfully", book: result });
+      .json({
+        message: "Seat Reservation added successfully",
+        reservation: result,
+      });
   },
   view: async (req, res) => {
     const bookId = req.query.id;
-   
-    const result = await SeatsReservations.view(bookId,req.organization_ids);
+
+    const result = await SeatsReservations.view(bookId, req.organization_ids);
     if (result.error) {
       return res.status(404).json({ error: result.message });
     }
-    if(result.length === 0) {
+    if (result.length === 0) {
       return res.status(404).json({ mess: "No Seat Reservation found" });
     }
-    if(bookId) return res.status(200).json({...result[0] });
-    return res.status(200).json({ books: result});
+    if (bookId) return res.status(200).json({ ...result[0] });
+    return res.status(200).json({ reservations: result });
   },
   update: async (req, res) => {
     const bookData = req.body;
     if (!bookData.reservation_id) {
-      return res.status(400).json({ error: "Seat Reservation ID is required for update" });
+      return res
+        .status(400)
+        .json({ error: "Seat Reservation ID is required for update" });
     }
     const result = await SeatsReservations.update(bookData);
     if (result.error) {
       return res.status(500).json({ error: result.message });
     }
-    if(result.affectedRows === 0){
-        return res.status(404).json({ error: "Seat Reservation not found" });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Seat Reservation not found" });
     }
     return res
       .status(200)
-      .json({ message: "Seat Reservation updated successfully", book: result });
+      .json({
+        message: "Seat Reservation updated successfully",
+        reservation: result,
+      });
   },
-  delete: async (req,res)=>{
-     const reservation_id = req.query.id;
+  delete: async (req, res) => {
+    const reservation_id = req.query.id;
     //  console.log("Book ID to delete:", reservation_id);
-     const result = await SeatsReservations.delete(reservation_id);
-     if(result.error) {
-       return res.status(500).json({ error: result.message });
-     }
-     if(result.affectedRows === 0) {
-       return res.status(404).json({ error: "Seat Reservation not found" });
-     }
-     return res.status(200).json({ message: "Seat Reservation deleted successfully", reservation_id });
-  }
+    const result = await SeatsReservations.delete(reservation_id);
+    if (result.error) {
+      return res.status(500).json({ error: result.message });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Seat Reservation not found" });
+    }
+    return res
+      .status(200)
+      .json({
+        message: "Seat Reservation deleted successfully",
+        reservation_id,
+      });
+  },
 };
 
 export default SeatReservationController;
--- src/controllers/library_managment/seats.controllers.js
+++ src/controllers/library_managment/seats.controllers.js
@@ -13,16 +13,16 @@ const SeatController = {
   },
   view: async (req, res) => {
     const seat_id = req.query.id;
-   
-    const result = await SeatsModel.view(seat_id,req.organization_ids);
+
+    const result = await SeatsModel.view(seat_id, req.organization_ids);
     if (result.error) {
       return res.status(404).json({ error: result.message });
     }
-    if(result.length === 0) {
+    if (result.length === 0) {
       return res.status(404).json({ mess: "No Seat found" });
     }
-    if(seat_id) return res.status(200).json({...result[0] });
-    return res.status(200).json({ seats: result});
+    if (seat_id) return res.status(200).json({ ...result[0] });
+    return res.status(200).json({ seats: result });
   },
   update: async (req, res) => {
     const bookData = req.body;
@@ -31,27 +31,29 @@ const SeatController = {
     }
     const result = await SeatsModel.update(bookData);
     if (result.error) {
-      return res.status(500).json({ error: result.message });
+      return res.status(500).json({ error: result.error });
     }
-    if(result.affectedRows === 0){
-        return res.status(404).json({ error: "Seat not found" });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Seat not found" });
     }
     return res
       .status(200)
       .json({ message: "Seat updated successfully", book: result });
   },
-  delete: async (req,res)=>{
-     const seat_id = req.query.id;
+  delete: async (req, res) => {
+    const seat_id = req.query.id;
     //  console.log("Seat ID to delete:", seat_id);
-     const result = await SeatsModel.delete(seat_id);
-     if(result.error) {
-       return res.status(500).json({ error: result.message });
-     }
-     if(result.affectedRows === 0) {
-       return res.status(404).json({ error: "Seat not found" });
-     }
-     return res.status(200).json({ message: "Seat deleted successfully", seat_id });
-  }
+    const result = await SeatsModel.delete(seat_id);
+    if (result.error) {
+      return res.status(500).json({ error: result.message });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Seat not found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Seat deleted successfully", seat_id });
+  },
 };
 
 export default SeatController;
