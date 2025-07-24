fb03f3a1d6cb74209d5e8e208eb2298489193133
Rahul Kumar Bharti <rk2255p@gmail.com>
2025-07-24 20:24:19 +0530

    Authenticated Organization Routes

--- src/controllers/organizations/organizations.contollers.js
+++ src/controllers/organizations/organizations.contollers.js
@@ -13,45 +13,49 @@ const OrganizationController = {
   },
   view: async (req, res) => {
     const created_by_admin = req.query.id;
-   
+
     const result = await OrganizationsModel.view(created_by_admin);
     if (result.error) {
       return res.status(404).json({ error: result.message });
     }
-    if(result.length === 0) {
+    if (result.length === 0) {
       return res.status(404).json({ mess: "No Organization found" });
     }
     // if(created_by_admin) return res.status(200).json({...result[0] });
-    return res.status(200).json({ organizations: result});
+    return res.status(200).json({ organizations: result });
   },
   update: async (req, res) => {
     const bookData = req.body;
     if (!bookData.organization_id) {
-      return res.status(400).json({ error: "Organization ID is required for update" });
+      return res
+        .status(400)
+        .json({ error: "Organization ID is required for update" });
     }
     const result = await OrganizationsModel.update(bookData);
     if (result.error) {
       return res.status(500).json({ error: result.message });
     }
-    if(result.affectedRows === 0){
-        return res.status(404).json({ error: "Organization not found" });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Organization not found" });
     }
     return res
       .status(200)
       .json({ message: "Organization updated successfully", book: result });
   },
-  delete: async (req,res)=>{
-     const organization_id = req.query.id;
+  delete: async (req, res) => {
+    const organization_id = req.query.id;
     //  console.log("Book ID to delete:", organization_id);
-     const result = await OrganizationsModel.delete(organization_id);
-     if(result.error) {
-       return res.status(500).json({ error: result.message });
-     }
-     if(result.affectedRows === 0) {
-       return res.status(404).json({ error: "Organization not found" });
-     }
-     return res.status(200).json({ message: "Organization deleted successfully", organization_id });
-  }
+    const result = await OrganizationsModel.delete(organization_id);
+    if (result.error) {
+      return res.status(500).json({ error: result.message });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Organization not found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Organization deleted successfully", organization_id });
+  },
 };
 
 export default OrganizationController;
--- src/routes/organizations.routes.js
+++ src/routes/organizations.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import OrganizationController from "../controllers/organizations/organizations.contollers.js";
+import authenticate from "../middlewares/authenticate.middleware.js";
 const router = express.Router();
 
-router.post("/", OrganizationController.add);
+router.post("/", authenticate, OrganizationController.add);
 router.get("/", OrganizationController.view);
-router.put("/", OrganizationController.update);
-router.delete("/", OrganizationController.delete);
+router.put("/", authenticate, OrganizationController.update);
+router.delete("/", authenticate, OrganizationController.delete);
 
 export default router;
